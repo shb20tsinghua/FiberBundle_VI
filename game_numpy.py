c3be11dfac54e258708371894ec41aab5a90d00d
shb20tsinghua <160827516+shb20tsinghua@users.noreply.github.com>
2026-09-22 20:38:00 +0800

    Update game_numpy.py

--- game_numpy.py
+++ game_numpy.py
@@ -2,14 +2,14 @@ import numpy as np
 from scipy.optimize.elementwise import find_root
 
 
-class Var_Ineq_game:
+class VI_simplex_product:
     def __init__(self, VI_F):
         self.VI_F = VI_F
 
-    def solve(self, pi_init, gap_tol, maxnit=50000, maxsubnit=10, verbose=0, record_file=None, print_preci=6):
-        self.nit, self.maxnit, self.maxsubnit, self.gap_TOL = 0, maxnit, maxsubnit, gap_tol
+    def solve(self, iter_init, gap_tol, efficient_corrector=True, maxnit=50000, maxsubnit=10, verbose=0, record_file=None, print_preci=6):
+        self.nit, self.maxnit, self.maxsubnit, self.gap_TOL, self.efficient_corrector = 0, maxnit, maxsubnit, gap_tol, efficient_corrector
         self.verbose, self.record_file, self.print_preci = verbose if record_file else 0, record_file, print_preci
-        self.m, self.n, self.mn = *pi_init.shape, pi_init.shape[0]*pi_init.shape[1]
+        self.m, self.n, self.mn = *iter_init.shape, iter_init.shape[0]*iter_init.shape[1]
         self.tau_min = min(1e-9, self.gap_TOL/self.mn)
         if verbose >= 1:
             open(record_file, 'w')
@@ -17,7 +17,7 @@ class Var_Ineq_game:
                 print_len = 6+print_preci
                 fio.writelines(f"|{'nit':^7}|{'gap':^{print_len}}|{'tau_sum':^{print_len}}|{'G_norm':^{print_len}}|{'d_sign':^7}|{'subnit':^7}|{'tang_stepln':^{print_len}}|{'avoid_n':^7}|\n")
         try:
-            pi = self.path_following(pi_init)
+            pi = self.path_following(iter_init)
         except UserWarning as err:
             print(err)
             pi = None
@@ -66,12 +66,12 @@ class Var_Ineq_game:
         J_pi = _I_1pi.dot(JF.reshape(self.mn, self.mn)*pi.flatten()[None, :]+np.diag(np.matvec(I_1pi, F).flatten())).dot(_I_1pi)
         return F, r_difference, tau_check_isum, I_1pi, J_pi
 
-    def corrector_comp(self, pi, tau, newton1=False):
+    def corrector_comp(self, pi, tau):
         F, r_difference, tau_check_isum, I_1pi, J_pi = self.state_comp(pi)
         tau_isum = tau.sum(axis=-1)
         G_pi_tau_norm = np.linalg.norm(np.matvec(I_1pi.swapaxes(1, 2), pi*F-tau).flatten())
         J_G = J_pi+np.diag(tau_isum.repeat(self.n))
-        if newton1:
+        if self.efficient_corrector:
             G_tilde_pi_tau = np.matvec(I_1pi, F-tau/pi).flatten()
             corrector_pi = np.linalg.solve(J_G.T.dot(J_G)+min(1, G_pi_tau_norm/self.mn)*np.eye(self.mn), J_G.T.dot(G_tilde_pi_tau/max(1, np.linalg.norm(G_tilde_pi_tau))))
         else:
@@ -80,14 +80,14 @@ class Var_Ineq_game:
         corrector_pi = np.matvec(I_1pi, corrector_pi.reshape(self.m, self.n))
         return corrector_pi, G_pi_tau_norm, tau_check_isum
 
-    def predictor_corrector(self, pi, tau_isum, decrease_only=False):
+    def predictor_corrector(self, pi, tau_isum, enforce_decrease=False):
         F, r_difference, tau_check_isum, I_1pi, J_pi = self.state_comp(pi)
         state_bkp = [item.copy() for item in [pi, tau_check_isum]]
         r = r_difference+(tau_isum-tau_check_isum)[:, None]
         tau = pi*r
 
         J_G = J_pi+np.diag(tau_isum.repeat(self.n))
-        dtau_sign = 1 if decrease_only else np.linalg.slogdet(J_G)[0]
+        dtau_sign = 1 if enforce_decrease else np.linalg.slogdet(J_G)[0]
         tangent_pi = np.linalg.solve(J_G, np.matvec(I_1pi, r).flatten())
         tangent_ln = np.sqrt(1+tangent_pi.dot(tangent_pi))
         eta = min(0.1, self.tangent_stepln/tangent_ln)
@@ -107,7 +107,8 @@ class Var_Ineq_game:
                 self.print_record()
             if (corrected := G_pi_tau_norm <= self.tau_min):
                 break
-            pi = (lambda vec: vec/vec.sum(axis=-1, keepdims=True))(np.exp(np.log(pi)-corrector_pi))
+            _pi = (lambda vec: vec/vec.sum(axis=-1, keepdims=True))(np.exp(np.log(pi)-corrector_pi))
+            pi = _pi if not np.isnan(_pi).any() else pi
             self.nit += 1
         if not corrected:
             pi, tau_check_isum, tau_isum_new = *state_bkp, tau_isum
